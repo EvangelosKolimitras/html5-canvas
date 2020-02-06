7e6ef2297583308c0556d3dda7303da433be589f
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2020-02-06 12:33:41 +0100

    added init() fn for spreading the shapes

--- index.js
+++ index.js
@@ -2,29 +2,64 @@ const canvas = document.querySelector("#canvas")
 const w = {
     width: window.innerWidth,
     height: window.innerHeight,
+    mouse: {
+        x: undefined,
+        y: undefined
+    },
+    colors: [
+        "red",
+        "blue",
+        "orange",
+        "pink",
+        "purple",
+        "yellow",
+        "green"
+    ]
 }
-canvas.width = w.width
-canvas.height = w.height
+
 const c = canvas.getContext('2d')
+
+window.addEventListener('mousemove', (e) => {
+    w.mouse.x = e.x
+    w.mouse.y = e.y
+
+})
+
+window.addEventListener('resize', () => {
+    canvas.width = w.width
+    canvas.height = w.height
+    init()
+})
+
+window.addEventListener('load', () => {
+    canvas.width = w.width
+    canvas.height = w.height
+})
+
+const maxRadius = 50
+
 class Shape {
     constructor(x, y, dx, dy, r) {
         this.x = x;
         this.y = y
         this.dx = dx
         this.dy = dy
-        this.r = r
+        this.color = w.colors[Math.floor(Math.random() * w.colors.length)]
     }
 }
 
 class Circle extends Shape {
     constructor(x, y, dx, dy, r) {
         super(x, y, dx, dy, r)
+        this.r = r
+        this.minR = r
     }
     draw = () => {
 
         c.beginPath()
         c.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false)
-        c.stroke()
+        c.fill()
+        c.fillStyle = this.color
     }
 
     update = () => {
@@ -38,20 +73,34 @@ class Circle extends Shape {
         this.x += this.dx
         this.y += this.dy
 
+        if (w.mouse.x - this.x < 100 && w.mouse.x - this.x > -100 && w.mouse.y - this.y < 100 && w.mouse.y - this.y > -100) {
+            if (this.r < maxRadius) {
+
+                this.r += 1
+            }
+        } else if (this.r > this.minR) {
+            this.r -= 1
+        }
+
         this.draw()
     }
 }
 
-const circlesArray = []
-for (let i = 0; i < 20; i++) {
-    const circle = [
-        Math.random() * (w.width - 40 * 2) + 40,
-        Math.random() * (w.height - 40 * 2) + 40,
-        (Math.random() - .5) * 5,
-        (Math.random() - .5) * 5,
-        40
-    ]
-    circlesArray.push(new Circle(...circle))
+let circlesArray = []
+
+const init = () => {
+    const speed = 5
+    circlesArray = []
+    for (let i = 0; i < 500; i++) {
+        const circle = [
+            Math.random() * (w.width - 40 * 2) + 40,
+            Math.random() * (w.height - 40 * 2) + 40,
+            (Math.random() - .5) * speed,
+            (Math.random() - .5) * speed,
+            Math.random() * 3 + 1
+        ]
+        circlesArray.push(new Circle(...circle))
+    }
 }
 const animate = () => {
     c.clearRect(0, 0, w.width, w.height)
@@ -60,5 +109,5 @@ const animate = () => {
         circlesArray[i].update()
     }
 }
-console.log(new Circle(1, 2, 3, 4, 10))
 animate()
+init()
